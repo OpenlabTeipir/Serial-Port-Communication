683955f713b7bdd97bb8e9c6c4aae5845fab31dc
Evaggelos Avgerinos <evaggelos.avgerinos@gmail.com>
2012-06-14 16:05:04 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+package gr.teipir.openlab.smarthome;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		USBCommunication com = new USBCommunication();
+		com.init();
+	}
+
+}
